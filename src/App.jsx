212fa220ee65bb1b0925952a52353a92b597edd5
Sushil <grayson.9999@gmail.com>
2024-12-11 20:53:14 +1000

    implementing footer and homepage design

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,9 @@ import { Login } from "./components/Login";
 
 function App() {
   return (
-    <div className="wrapper">
+    <div className="wrapper ">
       <h2 className="text-center m-2 p-3 title font-effect-shadow-multiple">
-        Tyreshop Management{" "}
-        {/* <span className="word-effect">
-          <span>M</span>
-          <span>a</span>
-          <span>n</span>
-          <span>a</span>
-          <span>g</span>
-          <span>e</span>
-          <span>m</span>
-          <span>e</span>
-          <span>n</span>
-          <span>t</span>
-        </span> */}
-        Application
+        Tyreshop Management Application
       </h2>
       <hr className="hr-line m-2" />
       <div className="homepage-container m-2 ">
@@ -31,6 +18,9 @@ function App() {
           {/* <div className="bouncing-tyre"></div> */}
         </div>
       </div>
+      <footer className="footer-section  m-2">
+        &copy; 2024 Copyright all reserved || Sushil Dangoriya
+      </footer>
     </div>
   );
 }
